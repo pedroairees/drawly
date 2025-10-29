c19a973ba5b0ed4a47a1d657f7d99f7594c5a674
pedroairees <pedrocoelho2209@gmail.com>
2025-10-29 19:17:09 -0300

    finished project

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const inputAmountNumbers = document.getElementById('numbers')
 const inputFrom = document.getElementById('from')
 const inputTo = document.getElementById('to')
 
+const checkboxRepeat = document.getElementById('repeat')
 const btnSubmit = document.getElementById('submit-btn')
 const btnAgain = document.querySelector('.draw-again')
 
@@ -11,6 +12,7 @@ const wrapperResponse = document.querySelector('.wrapper-response')
 
 const h2Form = document.querySelector('.content-form h2')
 const pForm = document.querySelector('.content-form p')
+const msgResult = document.querySelector('.show-result p')
 
 
 function gerarNumero(min, max) {
@@ -64,10 +66,13 @@ function toggleBtn() {
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault()
+    btnSubmit.disabled = true
 
     const amountNumbers = inputAmountNumbers.valueAsNumber
     const from = inputFrom.valueAsNumber
     const to = inputTo.valueAsNumber
+
+    const allowRepeat = !checkboxRepeat.checked
 
     // Verificação extra se os inputs estão preenchidos
     if(isNaN(amountNumbers) || isNaN(from) || isNaN(to)) {
@@ -79,18 +84,50 @@ form.addEventListener('submit', async (event) => {
     if(from >= to) {
         alert('O valor inicial não pode ser maior que o final!')
         inputFrom.focus()
+        btnSubmit.disabled = false
+        return
+    }
+
+    const avaliableNumbers = to - from + 1
+    if(!allowRepeat && amountNumbers > avaliableNumbers) {
+        alert(`Erro: Você pediu ${amountNumbers} números sem repetição, mas só existem ${avaliableNumbers} números disponíveis do intervalo de ${from} a ${to}.`)
+        inputAmountNumbers.focus()
+        btnSubmit.disabled = false
         return
     }
 
     wrapperResponse.innerHTML = ''
 
     const numbersToDraw = []
+    const drawnNumbers = new Set()
+
     for(let i = 0; i < amountNumbers; i++) {
-        numbersToDraw.push(gerarNumero(from, to))
+        if(allowRepeat) {
+            numbersToDraw.push(gerarNumero(from, to))
+        } else {
+            let numberGenerate
+
+            do{
+                numberGenerate = gerarNumero(from, to)
+            }while(drawnNumbers.has(numberGenerate))
+
+                drawnNumbers.add(numberGenerate)
+                numbersToDraw.push(numberGenerate)
+        }
+
+        
     }
 
+    let contador = 1
     for(const number of numbersToDraw) {
+        if(numbersToDraw.length === 1) {
+            msgResult.textContent = `resultado`
+        } else {
+            msgResult.textContent = `${contador}º resultado`
+        }
+
         await addResult(number)
+        contador++
     }
 
     toggleBtn()
@@ -102,4 +139,5 @@ btnAgain.addEventListener('click', (event) => {
     showFrom()
     btnAgain.classList.add('hide')
     btnSubmit.classList.remove('hide')
+    btnSubmit.disabled = false
 })
